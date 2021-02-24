b63e38fd024ae678927d550b0804c82745dc8a7e
James Sinkler <jamessinkler713@gmail.com>
2021-02-24 15:33:16 -0800

    updates to class based component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import './App.css';
+import { Component } from 'react';
 import Comment from './Comment';
 
-function App() {
-  // name and sotre variables before return statement
-  const post = {
+class App extends Component {
+  state = {
     title: "Dinosaurs are awesome",
     author: "Stealthy Stegosaurus",
     body: "Checkout out this body property",
-    comments: ['First!', 'Great post', 'Hire this author now!']
+    comments: ['First!', 'Great post', 'Hire this author now!'] 
   }
-  // map returns an array 
-  const comments = post.comments.map((comment, index)=> (<Comment text={comment} key={index}/>))
-  // comments = [ <Comment text={post.comments[0]} />, <Comment text={post.comments[1]} />, <Comment text={post.comments[1]} />]
 
-  return (
-    <div className="App">
-      <h1>{post.title}</h1>
-      <p>by {post.author}</p>
-      <p>{post.body}</p>
-      <h3>Comments:</h3>
-      <p>{post.comments[0]}</p>
-      { comments }
-    </div>
-  );
+  render() {
+    const post = {
+      title: "Dinosaurs are awesome",
+      author: "Stealthy Stegosaurus",
+      body: "Checkout out this body property",
+      comments: ['First!', 'Great post', 'Hire this author now!']
+    }
+    // map returns an array 
+    const comments = post.comments.map((comment, index)=> (<Comment text={comment} key={index}/>))
+ 
+    return (
+      <div className="App">
+        <h1>{this.state.title}</h1>
+        <p>by {post.author}</p>
+        <p>{post.body}</p>
+        <h3>Comments:</h3>
+        <p>{post.comments[0]}</p>
+        { comments }
+      </div>
+    );
+  }
 }
 
 export default App;
